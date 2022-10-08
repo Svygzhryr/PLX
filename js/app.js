d650a1a44f60cdf2c48e32f146c3491c08ef1693
Svygzhryr <pamixy@gmail.com>
2022-10-08 13:24:28 +0300

    the world

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 window.addEventListener('scroll', function(e) {
-    document.body.style.cssText = `--scrollTop: ${this.scrollY}px`
+    document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
 })
-
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create ({
+    wrapper: '.wrapper',
+    content: '.container'
+})
